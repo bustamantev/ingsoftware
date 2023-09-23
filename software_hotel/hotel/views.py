10328d542c7e4f92643d4dc14a3cac603d6031ac
IgneL462 <d.barrigamolina@gmail.com>
2023-09-23 20:05:17 -0300

    implementacion filtro por tipo de habitacion

--- software_hotel/hotel/views.py
+++ software_hotel/hotel/views.py
@@ -4,6 +4,7 @@ from django.http import Http404
 from django.contrib.auth.decorators import login_required
 from .models import Cliente, Administrador, Habitacion, Reserva, Tipo_habitacion
 from datetime import timedelta, datetime
+from .forms import formularioReserva
 
 
 def cliente_required(view_func):
@@ -31,7 +32,9 @@ def inicio(request):
 
 
 def busqueda(request):
-    return render(request, "busqueda.html")
+    form = formularioReserva
+    data = {'form':form}
+    return render(request, "busqueda.html", data)
 
 
 def resultados(request):
@@ -39,10 +42,12 @@ def resultados(request):
     fecha_entrada = None
     fecha_salida = None
     numero_huespedes = None
+    tipo_habitacion = None
     if request.method == "POST":
         fecha_entrada = request.POST.get("fecha_entrada")
         fecha_salida = request.POST.get("fecha_salida")
-        numero_huespedes = request.POST.get("numero_huespedes")
+        numero_huespedes = int(request.POST.get("numero_huespedes"))
+        tipo_habitacion = int(request.POST.get("tipo_habitacion"))
         if fecha_entrada != "":
             fecha_entrada = datetime.strptime(fecha_entrada, "%Y-%m-%d").date()
             if fecha_entrada < fecha_actual:
@@ -53,7 +58,6 @@ def resultados(request):
                 fecha_salida = datetime.strptime(
                     fecha_salida, "%Y-%m-%d").date()
                 if fecha_salida <= fecha_entrada:
-
                     return render(request, "busqueda.html", {
                         "error_fecha_salida": "La fecha de salida no puede ser anterior a la fecha de entrada",
                     })
@@ -61,17 +65,14 @@ def resultados(request):
                 fecha_salida = fecha_entrada + timedelta(days=1)
         else:
             fecha_salida = ""
-        if numero_huespedes != "":
-            numero_huespedes = int(numero_huespedes)
-            if numero_huespedes < 1 and numero_huespedes > 4:
-                return render(request, "busqueda.html", {
-                    "error_cantidad_huesped": "La cantidad de huesped debe estar en un rango de 1 a 4",
-                })
     if request.POST:
         habitaciones_disponibles = Habitacion.objects.all()
-        if numero_huespedes != "":
+        if numero_huespedes != 0:
             habitaciones_disponibles = habitaciones_disponibles.filter(
                 capacidad__exact=numero_huespedes)
+        if tipo_habitacion != 0:
+            habitaciones_disponibles = habitaciones_disponibles.filter(
+                tipo_habitacion__exact=tipo_habitacion)
         if fecha_entrada != "" and fecha_salida != "":
             reservas_existen = Reserva.objects.all().filter(
                 fecha_entrada__lte=fecha_salida, fecha_salida__gte=fecha_entrada)
@@ -79,6 +80,11 @@ def resultados(request):
                 for i in reservas_existen:
                     habitaciones_disponibles = habitaciones_disponibles.exclude(
                         numero_habitacion__exact=i.habitacion.numero_habitacion)
+        print('fecha_actual:',fecha_actual)
+        print('fecha_entrada:',fecha_entrada)
+        print('fecha_salida:',fecha_salida)
+        print('numero_huespedes:',numero_huespedes)
+        print('tipo_habitacion:',tipo_habitacion)
         return render(request, "resultados.html", {'habitaciones': habitaciones_disponibles})
 
 
@@ -93,8 +99,8 @@ def catalogo(request):
         servicios = list(tipo_habitacion.servicios.all().values('descripcion'))
         equipos = list(tipo_habitacion.equipos.all().values('descripcion'))
         data.append({'nombre': tipo_habitacion.nombre,
-                     'equipos': equipos,
-                     'servicios': servicios})
+                    'equipos': equipos,
+                    'servicios': servicios})
     return render(request, "catalogo.html", {'tipo_habitacion': data})
 
 
