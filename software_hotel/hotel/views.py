dd2ee6bdc9f0c097ec5154be08f206ad564fad32
IgneL462 <d.barrigamolina@gmail.com>
2023-10-06 20:31:23 -0300

    registro, inicio de sesion, cierre de sesion y cliente anonimo

--- software_hotel/hotel/views.py
+++ software_hotel/hotel/views.py
@@ -7,16 +7,6 @@ from datetime import timedelta, datetime
 from django.contrib.auth import login, logout, authenticate
 
 
-
-def cliente_required(view_func):
-    def _wrapped_view(request, *args, **kwargs):
-        if request.user.is_authenticated and isinstance(request.user, Cliente):
-            return view_func(request, *args, **kwargs)
-        else:
-            raise Http404("Página no encontrada")
-    return _wrapped_view
-
-
 def administrador_required(role):
     def decorator(view_func):
         def _wrapped_view(request, *args, **kwargs):
@@ -72,7 +62,7 @@ def resultados(request):
                 "error_numero_huesped": "debe ingresar cantidad de personas que se hospedaran",
             })
 
-    if request.POST:
+    if request.method == 'POST':
         habitaciones_disponibles = Habitacion.objects.all()
         habitaciones_disponibles = habitaciones_disponibles.filter(
             capacidad__gte=numero_huespedes)
@@ -98,10 +88,8 @@ def detalle(request):
     numero_huespedes = request.POST.get("numero_huespedes")
 
     habitacion = Habitacion.objects.get(habitacion_id=habitacion_id)
-    servicios = list(
-        habitacion.tipo_habitacion.servicios.all().values('descripcion'))
-    equipos = list(
-        habitacion.tipo_habitacion.equipos.all().values('descripcion'))
+    servicios = list(habitacion.tipo_habitacion.servicios.all().values('descripcion'))
+    equipos = list(habitacion.tipo_habitacion.equipos.all().values('descripcion'))
 
     date_in = datetime.strptime(fecha_entrada, '%d/%m/%Y').date()
     date_out = datetime.strptime(fecha_salida, '%d/%m/%Y').date()
@@ -117,6 +105,85 @@ def detalle(request):
             'precio_final': precio_final}
     return render(request, "detalle.html", data)
 
+def datos_cliente(request):
+    habitacion_id = request.POST.get("habitacion_id")
+    fecha_entrada = request.POST.get("fecha_entrada")
+    fecha_salida = request.POST.get("fecha_salida")
+    numero_huespedes = request.POST.get("numero_huespedes")
+    precio_final = request.POST.get("precio_final")
+    data = {'habitacion_id':habitacion_id,
+    'fecha_entrada':fecha_entrada,
+    'fecha_salida':fecha_salida,
+    'numero_huespedes':numero_huespedes,
+    'precio_final':precio_final,}
+    if request.user.is_authenticated:
+        correo = request.user.cliente.correo
+        data.update({'correo':correo})
+        return render(request, 'metodo_pago.html', data)
+    else:
+        return render(request,'datos_cliente.html', data)
+
+
+def datos_cliente_done(request):
+    if request.method == 'POST':
+        habitacion_id = request.POST.get("habitacion_id")
+        fecha_entrada = request.POST.get("fecha_entrada")
+        fecha_salida = request.POST.get("fecha_salida")
+        numero_huespedes = request.POST.get("numero_huespedes")
+        precio_final = request.POST.get("precio_final")
+        
+        correo = request.POST.get('correo')
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        telefono = request.POST.get('telefono')
+        
+        flag = True
+        data = {'habitacion_id':habitacion_id,
+            'fecha_entrada':fecha_entrada,
+            'fecha_salida':fecha_salida,
+            'numero_huespedes':numero_huespedes,
+            'precio_final':precio_final,
+            
+            'correo':correo,
+            'nombre':nombre,
+            'apellido':apellido,
+            'telefono':telefono}
+        if not correo:
+            data.update({'error_correo':'Debe ingresar un correo'})
+            flag = False
+
+        try:
+            cliente = Cliente.objects.get(username = correo)
+            use = authenticate(request,username=cliente.username, password='')
+            if use is None:
+                data.update({'error_correo':'El correo ya se encuentra registrado'})
+                flag = False
+        except:
+            pass
+        try:
+            administrador = Administrador.objects.get(username=correo)
+            data.update({'error_correo':'El correo ya se encuentra registrado'})
+            flag = False
+        except:
+            pass
+        if not nombre:
+            data.update({'error_nombre':'Debe ingresar un nombre'})
+            flag = False
+        if not apellido:
+            data.update({'error_apellido':'Debe ingresar un apellido'})
+        if not telefono: 
+            data.update({'error_telefono':'Debe ingresar un numero de telefono'})
+            flag = False
+        if len(telefono) != 9 or not telefono.isdigit() :
+            data.update({'error_telefono':'El telefono ingresado no es valido, debe tener solo 9 digitos.'})
+            flag = False
+        if not flag:
+            return render(request, 'datos_cliente.html', data)
+        else:
+            return render(request, 'metodo_pago.html', data)
+
+
+
 
 def metodo_pago(request):
     habitacion_id = request.POST.get("habitacion_id")
@@ -124,13 +191,23 @@ def metodo_pago(request):
     fecha_salida = request.POST.get("fecha_salida")
     numero_huespedes = request.POST.get("numero_huespedes")
     precio_final = request.POST.get("precio_final")
+
+    correo = request.POST.get('correo')
+    nombre = request.POST.get('nombre')
+    apellido = request.POST.get('apellido')
+    telefono = request.POST.get('telefono')
+
     data = {'habitacion_id':habitacion_id,
-            'fecha_entrada':fecha_entrada,
-            'fecha_salida':fecha_salida,
-            'numero_huespedes':numero_huespedes,
-            'precio_final':precio_final
-            }
+        'fecha_entrada':fecha_entrada,
+        'fecha_salida':fecha_salida,
+        'numero_huespedes':numero_huespedes,
+        'precio_final':precio_final,
+        'correo':correo,
+        'nombre':nombre,
+        'apellido':apellido,
+        'telefono':telefono}
     return render(request, "metodo_pago.html",data)
+
 
 
 def realizado(request):
@@ -139,6 +216,11 @@ def realizado(request):
     fecha_salida = request.POST.get("fecha_salida")
     numero_huespedes = request.POST.get("numero_huespedes")
     precio_final = request.POST.get("precio_final")
+
+    correo = request.POST.get('correo')
+    nombre = request.POST.get('nombre')
+    apellido = request.POST.get('apellido')
+    telefono = request.POST.get('telefono')
     if request.method == "POST":
         metodo_pago = int(request.POST.get("metodo_pago"))
         if metodo_pago != 0:
@@ -148,14 +230,33 @@ def realizado(request):
             precio_final = int(precio_final)
             metodo_pago = Metodo_pago.objects.get(pago_id=metodo_pago)
             habitacion = Habitacion.objects.get(habitacion_id=habitacion_id)
-            
+            if not request.user.is_authenticated:
+                cliente = Cliente(
+                    username = correo,
+                    correo = correo,
+                    nombre = nombre,
+                    apellido = apellido,
+                    telefono = telefono
+                )
+                try:
+                    cliente = Cliente.objects.get(username = correo)
+                    use = authenticate(request,username=cliente.username, password='')
+                    if use is None:
+                        cliente.set_password('')
+                        cliente.save()
+                except:
+                    cliente.set_password('')
+                    cliente.save()
+            else:
+                cliente = Cliente.objects.get(username=request.user.cliente.username)
             reserva = Reserva(
                 fecha_entrada = fecha_entrada,
                 fecha_salida = fecha_salida,
                 cantidad_personas = numero_huespedes,
                 precio_final = precio_final,
                 pago = metodo_pago,
-                habitacion = habitacion
+                habitacion = habitacion,
+                cliente = cliente
             )
             reserva.save()
             return render(request, "realizado.html")
@@ -163,7 +264,6 @@ def realizado(request):
             return render(request, "metodo_pago.html", {
                 "error_metodo_pago": "Debe ingresar un metodo de pago antes de continuar",
             })
-
 
 def catalogo(request):
     tipos_habitaciones = Tipo_habitacion.objects.all()
@@ -176,7 +276,104 @@ def catalogo(request):
                     'servicios': servicios})
     return render(request, "catalogo.html", {'tipo_habitacion': data})
 
+def registro_cli(request):
+    return render(request, 'registro_cli.html')
+    pass
 
+def registro_cli_done(request):
+    if request.method == 'POST':
+        correo = request.POST.get('correo')
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        telefono = request.POST.get('telefono')
+        vip = request.POST.get('vip')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        flag = True
+        data={'correo':correo,
+            'nombre':nombre,
+            'apellido':apellido,
+            'telefono':telefono}
+        if not correo:
+            data.update({'error_correo':'Debe ingresar un correo'})
+            flag = False
+        else:
+            try:
+                cliente = Cliente.objects.get(correo = correo)
+                #validar si el cliente es anonimo
+                data.update({'error_correo':'El correo ya se encuentra registrado'})
+                flag = False
+            except:
+                pass
+            try:
+                administrador = Administrador.objects.get(correo=correo)
+                data.update({'error_correo':'El correo ya se encuentra registrado'})
+                flag = False
+            except:
+                pass
+        if not nombre:
+            data.update({'error_nombre':'Debe ingresar un nombre'})
+            flag = False
+        if not apellido:
+            data.update({'error_apellido':'Debe ingresar un apellido'})
+        if not telefono: 
+            data.update({'error_telefono':'Debe ingresar un numero de telefono'})
+            flag = False
+        if len(telefono) != 9 or not telefono.isdigit() :
+            data.update({'error_telefono':'El telefono ingresado no es valido, debe tener solo 9 digitos.'})
+            flag = False
+        if not vip:
+            vip = False
+        else:
+            vip = True
+        if not password:
+            data.update({'error_password':'Debe ingresar una contraseña'})
+            flag = False
+        else:
+            if not password2:
+                data.update({'error_password':'Debe ingresar repetir contraseña'})
+                flag = False
+            else:
+                if password != password2:
+                    data.update({'error_password':'Las contraseñas no coinciden'})
+                    flag = False
+        if not flag:
+            return render(request, 'registro_cli.html', data)
+        else:
+            cliente = Cliente(username = correo,
+                            correo = correo,
+                            nombre = nombre,
+                            apellido = apellido,
+                            telefono = telefono,
+                            vip = vip)
+            cliente.set_password(password)
+            cliente.save()
+            login(request, cliente)
+            return redirect('inicio')
+
+def iniciar_sesion_cli(request):
+    return render(request, 'iniciar_sesion_cli.html')
+
+def iniciar_sesion_cli_done(request):
+    if request.method == 'POST':
+        username = request.POST.get('correo')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            data = {'correo': username,
+                    'error': 'Correo y/o contraseña invalidos'}
+            return render(request, 'iniciar_sesion_cli.html', data)
+
+
+def cerrar_sesion_cli(request):
+    logout(request)
+    return redirect('inicio')
+
+
+#ADMINISTRACION
 def inicio_sesion_adm(request):
     return render(request,'administracion/inicio_sesion_adm.html')
 
@@ -357,7 +554,58 @@ def editar_reserva(request):
     return render(request, 'administracion/editar_reserva.html', data)
 
 def editar_reserva_done(request):
+    if request.method == 'POST':
+        reserva_id = request.POST.get('reserva_id')
+        fecha_entrada = request.POST.get('fecha_entrada')
+        fecha_salida = request.POST.get('fecha_salida')
+        cantidad_personas = request.POST.get('cantidad_personas')
+        numero_habitacion = request.POST.get('numero_habitacion')
+        precio_final = request.POST.get('precio_final')
+        correo_cliente = request.POST.get('correo')
+        
+        reserva = Reserva.objects.get(reserva_id = reserva_id)
+        habitacion = Habitacion.objects.get(numero_habitacion = numero_habitacion)
+        cliente = Cliente.objects.get(username = correo_cliente)
+        
+        reserva.fecha_entrada = fecha_entrada
+        reserva.fecha_salida = fecha_salida
+        reserva.cantidad_personas = cantidad_personas
+        reserva.habitacion = habitacion
+        reserva.precio_final = precio_final
+        reserva.cliente = cliente
+        
+        reserva.save()
+        return redirect('lista_reserva')
+
+def eliminar_reserva_done(request):
+    if request.method == 'POST':
+        reserva_id = request.POST.get('reserva_id')
+        reserva = Reserva.objects.get(reserva_id = reserva_id)
+        reserva.delete()
+        return redirect('lista_reserva')
+
+@login_required
+def listar_reserva_equipos(request):
+    reservas = Reserva.objects.all()
+    data = {'reservas':reservas}
+    return render(request, 'administracion/listar_reserva_equipos.html', data)
+
+
+def ver_reserva_equipo(request):
+    if request.method == 'POST':
+        reserva_id = request.POST.get('reserva_id')
+        reserva = Reserva.objects.get(reserva_id = reserva_id)
+        servicios = list(reserva.habitacion.tipo_habitacion.servicios.all().values('descripcion'))
+        equipos = list(reserva.habitacion.tipo_habitacion.equipos.all().values('descripcion'))
+
+        data = {'reserva':reserva,
+                'servicios':servicios,
+                'equipos':equipos}
+        return render(request, 'administracion/ver_reserva_equipo.html', data)
     pass
+
+
+
 
 
 @login_required
